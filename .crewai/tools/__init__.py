1692809b388c61589ece93005676d896af9ab0ae
Clayton Young <claytoneyoung+github@gmail.com>
2026-01-18 19:21:51 -0500

    feat: Add tools package initialization

---
+++ .crewai/tools/__init__.py
@@ -0,0 +1,17 @@
+"""CrewAI Tools for GitHub integration and code analysis."""
+
+from .github_tools import (
+    CommitDiffTool,
+    CommitInfoTool,
+    FileContentTool,
+    PRCommentTool,
+)
+from .related_files_tool import RelatedFilesTool
+
+__all__ = [
+    "CommitDiffTool",
+    "CommitInfoTool",
+    "FileContentTool",
+    "PRCommentTool",
+    "RelatedFilesTool",
+]
